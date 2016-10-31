cfd7dbaac7b2b543245596188efd2416126dd518
Ji Kim <jkim55@gmail.com>
2016-10-31 13:40:49 -0600

    initial commit

--- CodersAgainstHumanity/LoginVC.swift
+++ CodersAgainstHumanity/LoginVC.swift
@@ -2,16 +2,17 @@
 //  ViewController.swift
 //  CodersAgainstHumanity
 //
-//  Created by Ji Kim on 10/31/16.
+//  Created by ThunderWhatever g31 on 10/31/16.
 //  Copyright © 2016 ThunderWhatever. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.black
         // Do any additional setup after loading the view, typically from a nib.
     }
 
